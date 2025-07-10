01e005fe6cc59dada404753d69fc34f43920d62b
ndirie <nourdroidsoft@gmail.com>
2025-07-10 19:00:47 +0300

    updated...

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://your-domain.com', // replace with your actual site URL
+  generateRobotsTxt: true, // (optional)
+};
